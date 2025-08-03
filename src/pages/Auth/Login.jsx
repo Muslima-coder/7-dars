92a8d5116fabcbbcb2f1705dca0ea3b640715e03
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-08-03 13:36:10 +0500

    update commit

---
+++ src/pages/Auth/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Heading } from '../../components'
+
+const Login = () => {
+  return (
+    <>
+      <Heading type={"h1"} title={"Log in to Twitter"}/>
+    </>
+  )
+}
+
+export default Login
